8f0693fbb0204274a31f8319a7874ef752d562c8
masakazu-ishida <mishida@support-s-frontier.com>
2025-06-12 12:37:36 +0900

    プログラムをアップ

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,6 +3,18 @@ public class MyClass {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("");
+		int a  = 0;
+		while(true) {
+			System.out.println(a);
+			a++;
+			
+			if(a > 100) {
+				break;
+			}
+			
+		}
+		
 	}
 
 }
