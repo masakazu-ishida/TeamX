b9844e5b56f6d95bd3b29e3d1ef35a5904f6c001
masakazu-ishida <49050835+masakazu-ishida@users.noreply.github.com>
2025-06-12 12:39:21 +0900

    Add files via upload

--- src/MyClass.java
+++ src/MyClass.java
@@ -4,12 +4,12 @@ public class MyClass {
 	public static void main(String[] args) {
 		
 		System.out.println("");
-		int a  = 0;
+		int a  = 1000;
 		while(true) {
 			System.out.println(a);
-			a++;
+			a--;
 			
-			if(a > 100) {
+			if(a < 0) {
 				break;
 			}
 			
